452663340177ea68d410ef9a5d1ba9e429f8cc45
HD152637 <hong00315@naver.com>
2017-05-24 13:00:00 +0900

    실습과제10 - We Are the World2!!

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+import org.dimigo.inheritance.Chinese;
+import org.dimigo.inheritance.Japanese;
+import org.dimigo.inheritance.Korean;
+import org.dimigo.inheritance.Person;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ * 	 |_ PersonTest2
+ *
+ * 1. 개요 : 실습과제10 - We Are the World2!!
+ * 2. 작성일 : 2017. 5. 24.
+ * </pre>
+ * 
+ * @author		: HD152637
+ * @version		: 1.0
+**/
+
+public class PersonTest2 {
+
+	public static void main(String[] args) {
+		Person[] persons = {		
+			new Person("Tom"),
+			new Korean("홍길동"),
+			new Japanese("다나카"),
+			new Chinese("왕밍")
+		};
+		for (Person p : persons){
+			greeting(p);
+		}
+	}
+
+	private static void greeting(Person p){
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+		System.out.println();
+	}
+}
